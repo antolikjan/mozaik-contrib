74b979b85357c1fd68adff4817e568dde1596604
Jan Antolik <antolikjan@gmail.com>
2021-03-17 16:28:02 +0100

    added LSV1M_NEW: few minor improvements to the LSV1M model parametrization

---
+++ LSV1M_NEW/run_parameter_search.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import sys
+from mozaik.meta_workflow.parameter_search import CombinationParameterSearch, SlurmSequentialBackend, SlurmSequentialBackendMPI
+import numpy
+import time
+
+
+if True:
+    CombinationParameterSearch(SlurmSequentialBackend(num_threads=16, num_mpi=1,slurm_options=['--hint=nomultithread'],path_to_mozaik_env='/home/antolikjan/virt_env/mozaik/bin/activate'), {
+      'trial' : [1],
+      'sheets.retina_lgn.params.density' : [100,50,30],
+    }).run_parameter_search()
+
+
+
